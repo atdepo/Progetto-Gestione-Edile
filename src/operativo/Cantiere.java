f4a5aed29b2adebb84868998de8162913fda4c90
TheDMG8 <44084060+TheDMG8@users.noreply.github.com>
2020-01-13 16:59:26 +0100

    Create Cantiere.java

---
+++ src/operativo/Cantiere.java
@@ -0,0 +1,17 @@
+package operativo;
+
+import java.util.ArrayList;
+
+import dipendenti.Responsabile;
+/**
+ * Questa classe cattura il concetto  di un Cantiere.
+ * Ogni Cantiere possiede un Resposabile 
+ * @author 732438
+ *
+ */
+public class Cantiere {
+	private Responsabile responsabile;
+	private double valore;
+	private ArrayList<Squadra> Squadre;
+
+}
