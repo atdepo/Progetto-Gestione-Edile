cdb00ae3bf15053d252022f35504375703195c7a
TheDMG8 <44084060+TheDMG8@users.noreply.github.com>
2020-01-16 15:25:22 +0100

    Update RepartoAmministrativo.java

--- src/amministrativo/RepartoAmministrativo.java
+++ src/amministrativo/RepartoAmministrativo.java
@@ -8,6 +8,7 @@ import dipendenti.Dirigente;
 import dipendenti.Impiegato;
 import dipendenti.Operaio;
 import dipendenti.Quadro;
+import dipendenti.Operaio.lavoro;
 
 /**
  * Questa classe cattura il concetto  di un RepartoAmministrativo di un cantiere.
@@ -16,7 +17,7 @@ import dipendenti.Quadro;
  */
 public class RepartoAmministrativo {
 	
-	//questa è una responsabilità che deve avere la classe dipendente?
+	//questa ï¿½ una responsabilitï¿½ che deve avere la classe dipendente?
 	
 	protected static final double STIPENDIO_IMPIEGATO=7.0D;
 	protected static final double STIPENDIO_OPERAIO_GIORNO=25.0D;
@@ -76,6 +77,36 @@ public class RepartoAmministrativo {
 		
 	}
 	
+	public void assumiDirigente(String nome, String cognome, int eta) {
+		numDirigenti++;
+		dipendenti.add(new Dirigente(nome, cognome, eta, Integer.toString(numDirigenti)));
+	}
+	
+	public void assumiQuadro(String nome, String cognome, int eta) {
+		numQuadri++;
+		dipendenti.add(new Quadro(nome, cognome, eta, Integer.toString(numQuadri)));
+	}
+	
+	public void assumiOperaio(String nome, String cognome, int eta,lavoro specializzazione,int ore) {
+		numOperai++;
+		dipendenti.add(new Operaio(nome, cognome, eta, Integer.toString(numOperai), specializzazione, ore));
+	}
+	
+	public void assumiOperaio(String nome, String cognome, int eta,lavoro specializzazione) {
+		numOperai++;
+		dipendenti.add(new Operaio(nome, cognome, eta, Integer.toString(numOperai), specializzazione));
+	}
+	
+	public void assumiImpiegato(String nome, String cognome, int eta,String matricola,int giorni) {
+		numImpiegati++;
+		dipendenti.add(new Impiegato(nome, cognome, eta, Integer.toString(numImpiegati),giorni));
+	}
+	
+	public void assumiImpiegato(String nome, String cognome, int eta,String matricola) {
+		numImpiegati++;
+		dipendenti.add(new Impiegato(nome, cognome, eta, Integer.toString(numImpiegati)));
+	}
+	
 	static double pagaDirigente(Dipendente d) {
 		Contratto c=d.getContratto();
 		Dirigente dip=(Dirigente)d;
@@ -112,20 +143,22 @@ public class RepartoAmministrativo {
 		return saldo;
 	}
 	
-	public double pagamentoAssunti() {
-		double speseDipendentiAzienda=0;
-		for(int i=0;i<dipendenti.size();i++) {
-			Dipendente d=dipendenti.get(i);
-					
+	public int pagamentoAssunti() {
+		int speseDipendentiAzienda=0;
+		for(Dipendente d:dipendenti) {	
+			
 			if(Dipendente.isDirigente(d)) {
 				speseDipendentiAzienda+=pagaDirigente(d);
 			}
+			
 			else if(Dipendente.isImpiegato(d)) {
 				speseDipendentiAzienda+=pagaImpiegato(d);
 			}
+			
 			else if(Dipendente.isOperaio(d)) {
 				speseDipendentiAzienda+=pagaOperaio(d);
 			}
+			
 			else if(Dipendente.isQuadro(d)) {
 				speseDipendentiAzienda+=pagaQuadro(d);
 			}		
@@ -153,16 +186,9 @@ public class RepartoAmministrativo {
 		return numImpiegati;
 	}
 	
-	public Dipendente licenziamentoDipendente(String codiceDipendente) {
-		Dipendente rimosso = null;
-		for(int i=0;i<dipendenti.size();i++) {
-			Dipendente d=dipendenti.get(i);
-			if(d.getCodiceDipendente().equals(codiceDipendente))
-				rimosso=dipendenti.remove(i);
-		}
-	return rimosso;
+	public Dipendente licenziamentoDipendente(Dipendente daRimuovere) {
+		return dipendenti.remove(dipendenti.indexOf(daRimuovere));
 	}
-	
 	
 	
 }
